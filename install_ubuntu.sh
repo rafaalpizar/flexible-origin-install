752703bcc46db234f5a0d9fb1c99ad86f75ba394
Rafael Alpizar <ralpizar@akamai.com>
2023-01-23 16:36:35 -0600

    Remove sudo command because this script runs as root

--- install_ubuntu.sh
+++ install_ubuntu.sh
@@ -52,6 +52,6 @@ iptables -P INPUT DROP
 iptables -P FORWARD DROP
 iptables -P OUTPUT ACCEPT
 
-echo iptables-persistent iptables-persistent/autosave_v4 boolean true | sudo debconf-set-selections
-echo iptables-persistent iptables-persistent/autosave_v6 boolean true | sudo debconf-set-selections
+echo iptables-persistent iptables-persistent/autosave_v4 boolean true | debconf-set-selections
+echo iptables-persistent iptables-persistent/autosave_v6 boolean true | debconf-set-selections
 apt install -y iptables-persistent
